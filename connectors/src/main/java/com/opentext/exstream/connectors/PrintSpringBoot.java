18a4433ed5deda81c6f047eeb0008126ebbf379b
sila <sila@DCI5CG1242H4X.gsinet.be>
2022-03-18 15:01:52 +0100

    first commit

---
+++ connectors/src/main/java/com/opentext/exstream/connectors/PrintSpringBoot.java
@@ -0,0 +1,15 @@
+package com.opentext.exstream.connectors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintSpringBoot {
+	
+	
+	public static void main(String args[]){
+		
+		SpringApplication.run(PrintSpringBoot.class, args);
+	}
+
+}
